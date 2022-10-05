c0c4c2f3f2b9e24506515bde6050e436d84f8ab4
Darin Fisher <darinf@gmail.com>
2022-10-05 09:58:42 -0700

    Add skeleton project

---
+++ CardGrid/CardGrid/CardGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGridApp.swift
+//  CardGrid
+//
+//  Created by Darin Fisher on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct CardGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
