2ae20749f6fa020ee3a217c65b2a21ea93aee490
Darin Fisher <darinf@gmail.com>
2022-12-30 08:33:02 -0800

    snapshot

--- Browser/CardGrid/Views/SmallCardView.swift
+++ Browser/CardGrid/Views/SmallCardView.swift
@@ -8,13 +8,18 @@ struct SmallCardView<Card>: View where Card: CardModel {
     let selected: Bool
     let zoomed: Bool
     let handler: (_ action: CardView<Card>.Action) -> Void
+    @State var longPressed = false
 
     var body: some View {
         Button {
-            handler(.activated)
+            if longPressed {
+                longPressed = false
+            } else {
+                handler(.activated)
+            }
         } label: {
             Group {
-                if selected && zoomed {
+                if selected && zoomed || longPressed {
                     Color.clear
                 } else {
                     CardView(namespace: namespace, model: model, selected: selected, zoomed: false, handler: handler)
@@ -25,6 +30,31 @@ struct SmallCardView<Card>: View where Card: CardModel {
         }
         .buttonStyle(.reportsPresses(pressed: $model.pressed))
         .zIndex(selected ? 1 : 0)
+        .highPriorityGesture(
+            DragGesture()
+                .onChanged {
+                    print(">>> Drag onChanged: \($0.translation)")
+                }
+                .onEnded { _ in
+                    longPressed = false
+                }
+                .simultaneously(with: LongPressGesture()
+                    .onEnded { _ in
+                        longPressed = true
+                    }
+                    .sequenced(before: TapGesture()
+                        .onEnded {
+                            longPressed = false
+                        }
+                    )
+                )
+        )
+        .highPriorityGesture(TapGesture()
+            .onEnded {
+                longPressed = false
+                handler(.activated)
+            }
+        )
     }
 }
 
