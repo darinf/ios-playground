6291f42c2e1002df4cb7ccf1e59d98a1c19c16b7
Darin Fisher <darinf@gmail.com>
2024-08-21 11:52:19 -0700

    fix bug with switching back from incognito mode

--- BrowserProto/BrowserProto/WebContent/WebContentConfiguration.swift
+++ BrowserProto/BrowserProto/WebContent/WebContentConfiguration.swift
@@ -11,23 +11,20 @@ extension WebContentConfiguration {
         "Mozilla/5.0 (iPhone; CPU iPhone OS 17_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Mobile/15E148 Safari/604.1"
     }()
 
-    private static var normalConfiguration = {
+    private static var normalConfiguration = makeConfiguration(websiteDataStore: .default())
+    private static var incognitoConfiguration = makeConfiguration(websiteDataStore: .nonPersistent())
+
+    private static func makeConfiguration(websiteDataStore: WKWebsiteDataStore) -> WKWebViewConfiguration {
         let configuration = WKWebViewConfiguration()
         configuration.processPool = WKProcessPool()
         configuration.allowsInlineMediaPlayback = true
         configuration.ignoresViewportScaleLimits = true
-        configuration.websiteDataStore = .default()
+        configuration.websiteDataStore = websiteDataStore
 
         let controller = WKUserContentController()
         FaviconObserver.setup(userContentController: controller)
         configuration.userContentController = controller
 
         return configuration
-    }()
-
-    private static var incognitoConfiguration = {
-        let configuration = normalConfiguration
-        configuration.websiteDataStore = .nonPersistent()
-        return configuration
-    }()
+    }
 }
